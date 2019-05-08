e0a16fcdc394bf4bc11e0756dc811d4ceed0bbf2
nykevinwong <nykevinwong@users.noreply.github.com>
2019-05-08 15:26:03 -0400

    Update quicksort.java

--- quicksort.java
+++ quicksort.java
@@ -1,8 +1,124 @@
 // (1) find median [done, linear time complexity, but not best in the worst case (median of median is better)]
-// (2) 3 way quicksort/ducth national flag problem [https://algorithmsandme.com/dutch-national-flag-problem/]
-// (3) tail call optimization
-// (4) iterative quicksort
-// (5) median of medians 
+// (2) ducth national flag problem [done for 3 color] [https://algorithmsandme.com/3-way-quicksort/]
+// (3) simple 3 way quicksort [done]
+// (4) tail call optimization
+// (5) iterative quicksort
+// (6) median of medians 
+
+// 3-way parition: There is another quicksort optimization which is called as Entropy-optimal sorting or 3-way partition quick sort. 
+// This method is useful when inout array contains lot of duplicate values which is very frequent in real world.
+// Idea is to divide array in three parts rather than two. Let’s say P be pivot.
+// First part contains all numbers which are less than p, second part contains number equal to p and last part contains 
+// numbers which are greater than p.
+class QuickSort3WayPartition
+{
+    public long[] arr;
+
+    public QuickSort3WayPartition(int size)
+    {
+        arr = new long[size];
+        for(int i=0;i<arr.length;i++)
+            arr[i] = (int)(java.lang.Math.random()*199);
+    }
+
+    public void display()
+    {
+        System.out.println(java.util.Arrays.toString(this.arr));
+    }
+
+    public void swap(int index1, int index2)
+    {
+        long temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    public void quicksort()
+    {
+        quicksort(0, this.arr.length-1);
+    }
+
+    public void quicksort(int left, int right)
+    {
+        if(left >= right)
+        return;
+
+        long pivotValue = arr[left];
+        int reader = left+1;
+        int it = left;
+        int gt = right;
+
+        while(reader <= gt)
+        {
+            if(arr[reader] < pivotValue) swap(reader++,it++);
+            else if(arr[reader] == pivotValue) reader++;
+            else if(arr[reader] > pivotValue) swap(reader,gt--);
+        }
+        
+       
+        quicksort(left, it-1);
+        quicksort(gt+1, right);
+    }
+
+    public static void main(String[] args)
+    {
+        QuickSort3WayPartition q = new QuickSort3WayPartition(16);
+
+        q.display();
+        q.quicksort();
+        q.display();
+    }
+}
+
+// https://algorithmsandme.com/dutch-national-flag-problem/
+class DutchNationalFlag
+{
+    public static void swap(int[] nums, int a, int b)
+    {
+        int temp = nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
+    }
+
+    public static void solve3Color(int[] nums)
+    {
+        int lo, reader;
+        reader=lo=0;
+        int hi = nums.length-1;
+
+        System.out.println("\n3 color:" +java.util.Arrays.toString(nums));
+        while(reader<=hi)
+        {
+            if(nums[reader]==0) swap(nums,reader++,lo++);
+            else if(nums[reader]==1) reader++;
+            else if(nums[reader]==2) swap(nums,reader,hi--);
+        }
+        System.out.println(java.util.Arrays.toString(nums));
+    }
+
+    public static void solve4Color(int[] nums)
+    {
+        int lo, reader, mid;
+        reader=lo=mid=0;
+        int hi = nums.length-1;
+
+        System.out.println("\n4 color:" +java.util.Arrays.toString(nums));
+        while(reader<=hi)
+        {
+            if(nums[reader]==0)  { swap(nums,reader++,lo++); mid++; }
+            else if(nums[reader]==1) { reader++; mid++; }
+            else if(nums[reader]==2) { swap(nums,reader,mid); }
+            else if(nums[reader]==3) swap(nums,reader,hi--);
+        }
+        System.out.println(java.util.Arrays.toString(nums));
+    }
+
+    public static void main(String[] args)
+    {
+        solve3Color(new int[] {2,0,1,2,1,1,2,2,0,0,0,1,2,0});
+// not working        solve4Color(new int[] {2,0,1,3,2,1,3,1,2,2,3,3,0,3,0,3,0,1,2,0,2,1});
+    }
+}
 
 class QuickSelect
 {
@@ -387,3 +503,9 @@ class LomutoPartition
         System.out.println("dividing index position:" + pviotindex);
     }
 }
+
+/*
+A big limitation of quick sort is that it has O(n2) worst case running time. Some improvements have been suggested as given below:
+(1) Cutoff to insertion sort: Switch to insertion sort for arrays with size less than a pre-decided limit. We follow quick sort and partition array. Once, the size of partitioned array goes lower than limit, apply insertion sort on that array. Limit varies from system to system and typically it is between 5 to 15.
+(2) Median-of-three partitioning: Use median of a small sample of items taken from the array as the partitioning item. Doing so will give a slightly better partition, but at the cost of computing the median.
+*/
