75dce5f2ddc3acc49fa540a93069ebe87776e515
nykevinwong <nykevinwong@users.noreply.github.com>
2019-05-16 11:48:16 -0400

    Update mergesort.java

--- mergesort.java
+++ mergesort.java
@@ -1,5 +1,5 @@
 import java.util.*; 
-class Merge2SortedArray {  // Time: O(n), Space: O(m+n)
+class Merge2SortedArray {  // Time: O(m+n) => O(N), Space: O(m+n) => O(N)
    public static void main(String[] args)
       { // Arrays must be sorted before merging.
       int k = 5; // kth number
@@ -24,3 +24,39 @@ class Merge2SortedArray {  // Time: O(n), Space: O(m+n)
       while(j < m) r[k++] = sorted2[j++];  // sorted1 is empty, but sorted2 isn't
       }  // end merge()
    }  // end class MergeSortedArray
+class MergeCountFindKth {  // Time: O(m+n) => O(N), Space: O(1)
+// k-th element or median of Two Sorted Arrays of the same/different sizes
+   public static void main(String[] args)
+      { // Arrays must be sorted before merging.
+      int k = 5; // kth number
+      int[] arr1 = {23, 47, 81, 95};
+      int[] arr2 = {7, 14, 39, 55, 62, 74};
+      // watch out that kth is usualy 1-based value and array is zero-based. k-1 is the index for zero-based array.
+      System.out.println("3th smallest = " + findKth(arr1, 4, arr2, 6, k-1) );
+      int n = arr1.length + arr2.length;
+      long median = (n % 2 != 0) ? findKth(arr1, 4, arr2, 6, n/2) : (findKth(arr1, 4, arr2, 6, n/2 - 1) + findKth(arr1, 4, arr2, 6, n/2)  ) / 2 ;
+      System.out.println("median = " + median );
+      }  // end main()
+   //-----------------------------------------------------------
+     public static long findKth( int[] sorted1, int n, int[] sorted2, int m, int pos)
+      {  // merge two sorted arrays into one
+         int i=0, j=0, k=0; 
+
+         while(i < n && j < m) { // neither array empty
+            if( sorted1[i] < sorted2[j] ) {
+               if(k==pos) return sorted1[i];
+               i++; 
+            }
+            else {
+               if(k==pos) return sorted2[j];
+               j++; 
+            }
+            k++;
+         }
+
+         while(i < n) { if(k++==pos) return sorted1[i]; }
+         while(j < m) { if(k++==pos) return sorted2[j]; }
+
+         return Long.MIN_VALUE;
+      }  // end findKth()
+   }  // end class MergeCountFindKth
