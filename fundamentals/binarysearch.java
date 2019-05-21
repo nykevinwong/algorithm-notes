3913da26446b07bb6a3c7330f330e08bc954b3f8
nykevinwong <nykevinwong@users.noreply.github.com>
2019-05-21 14:24:45 -0400

    Update binarysearch.java

--- fundamentals/binarysearch.java
+++ fundamentals/binarysearch.java
@@ -34,6 +34,47 @@ class BinarySearchSortedArray {
    } // end iterativeBinarySearch
 }
 
+
+ // http://www.zrzahid.com/kth-smallest-element-in-two-sorted-arrays/
 class BinarySearch2SortedArray
-{ // http://www.zrzahid.com/kth-smallest-element-in-two-sorted-arrays/
+{
+    public static int findkth(int[] nums1, int[] nums2, int k)
+    {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int start = 0 ;
+        int end = java.lang.Math.min(n1, k);
+        
+        while(start <= end)
+        {
+            int mid1 = (start+end) / 2;
+            int mid2 = k - mid1;
+            if(mid2 > n2)
+            {
+                start = mid1+1; continue;
+            }
+
+            int l1 = (mid1 == 0) ? Integer.MIN_VALUE  : nums1[mid1-1];
+            int l2 = (mid2 == 0) ? Integer.MIN_VALUE : nums2[mid2-1];
+            int r1 = (mid1 == n1) ? Integer.MAX_VALUE : nums1[mid1];
+            int r2 = (mid2 == n2) ? Integer.MAX_VALUE : nums2[mid2];
+
+            if(l1 > r2) end = mid1 - 1;                
+            else if(l2 > r1) start = mid1 + 1;
+            else return java.lang.Math.max(l1, l2);
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] nums1 = new int[] { 1, 3, 6, 7, 10};
+        int[] nums2 = new int[] { 2, 4, 5, 8, 9};
+        int N = nums1.length + nums2.length;
+        int k1 = (N-1)/2 + 1; 
+        int k2 = (N)/2 + 1; 
+        double median = (findkth(nums1, nums2, k1) + findkth(nums1, nums2, k2) ) / 2.0;
+        System.out.println("median =" + median);
+    }
 }
