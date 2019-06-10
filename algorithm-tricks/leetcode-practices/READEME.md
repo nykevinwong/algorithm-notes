b39f8d8bec904e47787671888a494ae1dbdfd9f4
Kevin Wong <nyckevinwong@gmail.com>
2019-06-09 21:47:52 -0400

    added june 9th leetcode practices

---
+++ algorithm-tricks/leetcode-practices/READEME.md
@@ -0,0 +1,46 @@
+## June_01_2019.txt
+### 17. Letter Combinations of a Phone Number (Medium)
+### 771. Jewels and Stones (Easy)
+
+## June_02_2019.txt
+### 127. Word Ladder (Medium)
+
+## June_03_2019.txt
+### 11. Container With Most Water
+### 42. Trapping Rain Water (Hard)
+
+## June_04_2019.txt
+### 167. Two Sum II - Input array is sorted
+### 189. Rotate Array (Easy)
+### 13. Roman to Integer
+
+## June_06_2019.txt
+### 239. Sliding Window Maximum (Hard)
+
+## June_07_2019.txt
+### 67. Add Binary (Easy)
+
+## June_09_2019.txt
+### 9. Palindrome Number (Easy)
+### 125. Valid Palindrome (Easy)
+
+## May_28_2019.txt
+### 46. Permutations
+### 47. Permutations II (Medium)
+### 77. Combinations (Medium)
+### 78. Subsets [Medium]
+### 90. Subsets II (Medium)
+### 39. Combination Sum (Medium)
+
+## May_29_2019.txt
+### 40. Combination Sum II (Medium)
+
+## May_30_2019.txt
+### 216. Combination Sum III (Medium)
+### 22. Generate Parentheses (Medium)
+### 377. Combination Sum IV (Medium)
+
+## May_31_2019.txt
+### 567. Permutation in String (Medium)
+
+Total 23 problems are COMPLETED!!
---
+++ algorithm-tricks/leetcode-practices/countdone.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.nio.*;
+
+class countdone
+{
+    public static void main(String[] args) throws IOException
+    {
+        File dir = new File(".");
+        File[] filesList = dir.listFiles(new FilenameFilter() { 
+                 public boolean accept(File dir, String filename)
+                      { return filename.endsWith(".txt"); }
+        } );
+        
+
+        try
+        {
+            PrintWriter writer = new PrintWriter("READEME.md", "UTF-8");
+            System.out.println("Processing");
+            int count =0 ;
+            for (File file : filesList) {
+                if (file.isFile()) {
+                    writer.println("## " + file.getName());
+
+
+                        BufferedReader reader = new BufferedReader(new FileReader(file));
+                        String line = reader.readLine();
+                        while (line != null) {
+                            int pos = line.indexOf(".");
+
+                            if(pos > 0 && pos <= 10)
+                            {
+                                try
+                                {
+                                    String[] cols = line.split("\\.");
+                                    if(cols[0].length() <=5)
+                                    {
+                                        Integer result = Integer.valueOf(cols[0]);
+                                        if(result!=null )
+                                        {
+                                            writer.println("### " + line);
+                                            count++;
+                                        }	
+                                    }
+                                    System.out.print(".");
+                                }
+                                catch(Exception e)
+                                {
+                                    System.out.print("-");
+                                }
+
+                            }
+                            // read next line
+                            line = reader.readLine();
+                        }
+                        reader.close();
+                        writer.println("");
+
+                }
+            }
+
+            System.out.println("\nDone Processing!!");
+            writer.println("Total " + count + " problems are COMPLETED!!");
+            writer.close();
+        }
+        catch(Exception e)
+        {
+            System.out.println("error listing files.");
+            e.printStackTrace();
+        }
+
+        
+
+    }
+}
